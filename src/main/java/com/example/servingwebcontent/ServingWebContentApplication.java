f69d0a0bd0ab6c24c277ad453e81f8caba530dcb
minuta <gammamustang@gmail.com>
2021-05-05 00:53:37 +0200

    add logging features

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,13 +1,23 @@
 package com.example.servingwebcontent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(ServingWebContentApplication.class);
+
+	public static final String LOG_MSG = "Entering into SpringApplication...";
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServingWebContentApplication.class, args);
+
+		LOGGER.debug("DEBUG log message : " + LOG_MSG);
+		LOGGER.info("INFO log message : " + LOG_MSG);
+		LOGGER.error("ERROR log message : " + LOG_MSG);
 	}
 
 }
